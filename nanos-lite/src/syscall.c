5322574f81c9d8e1251db9c5f571e21301c2f2d8
fengjk <1327246148@qq.com>
2021-05-26 23:39:52 +0800

    pa3.2complete

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,6 @@
 #include <common.h>
 #include "syscall.h"
+
 enum {
     SYS_exit,
     SYS_yield,
@@ -22,10 +23,22 @@ enum {
     SYS_times,
     SYS_gettimeofday
 };
+
+int sys_write(int fd, void *buf, size_t count) {
+    int cnt = 0;
+    for (int i = 0; i < count; i++) {
+        putch(*(char *) (buf + i));
+        cnt++;
+    }
+    return cnt;
+}
+extern char end;
+
 void do_syscall(Context *c) {
     uintptr_t a[4];
     a[0] = c->GPR1;
     switch (a[0]) {
+
         case SYS_exit:
             halt(c->GPR2);
 
@@ -34,8 +47,16 @@ void do_syscall(Context *c) {
             c->GPRx = 0;
             yield();
             break;
+        case SYS_write:
+            c->GPRx = sys_write(c->GPR2, (void *)c->GPR3, c->GPR4);
+            break;
+        case SYS_brk:
+            end=end+c->GPR2;
+            c->GPRx=0;
+            break;
         default:
             panic("Unhandled syscall ID = %d", a[0]);
     }
 
 }
+
